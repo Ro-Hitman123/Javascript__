4d61404dd16837cb30422dd84b3a73ace30a5016
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 01:14:58 +0545

    fl

--- t4 Practice set.js
+++ t4 Practice set.js
@@ -1,5 +1,8 @@
-//Create a variable of type string and try to add a number to it. Observe the results.
+// Create a variable of type string and try to add a number to it. Observe the results.
 
 let a = "Kritagya"
 let b = 34
 console.log(a + b)  
+
+// Create typeof operator to find the datatype of the string in last question.
+
