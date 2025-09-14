87eda21bb1637a8d9624a5894ffb6392eba926ff
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 00:39:04 +0545

    dfjl

--- t1.js
+++ t1.js
@@ -1,1 +1,5 @@
 console.log("Hello from t1.js")
+var a = 67
+console.log(a)
+a = "Kritagya"
+console.log(a)
---
+++ t2.js
@@ -0,0 +1,8 @@
+// Var vs Let vs Const
+
+/*Var is function scoped.
+Let and Const are block scoped.
+Const cannot be reassigned.
+Let can be reassigned.
+Var can be reassigned.*/
+
