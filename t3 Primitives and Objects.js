7e1c63eb37bc8894236f8b9c800af1858c1e0826
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 01:01:08 +0545

    dfj

--- t3 Primitives and Objects.js
+++ t3 Primitives and Objects.js
@@ -1,4 +1,4 @@
-//Primitives
+//Primitives data types in JavaScript
 
 /*
 Null
@@ -10,3 +10,13 @@ BigInt
 Undefined
 */ 
 
+let a = null
+let b = 345
+let c = true
+let d = BigInt("567") + BigInt("3")  
+let e = "Kritagya"
+let f = Symbol("I am a nice symbol")
+let g = undefined
+
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
