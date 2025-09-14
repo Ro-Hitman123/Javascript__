885c1ee0c3720e2a2e229b2c906038397145bb56
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 00:47:25 +0545

    lfsjd

--- t2.js
+++ t2.js
@@ -6,3 +6,15 @@ Const cannot be reassigned.
 Let can be reassigned.
 Var can be reassigned.*/
 
+var a = 45
+console.log(a)  
+a = "Kritagya1"
+console.log(a)
+let b = "Kritagya2"
+const author = "Kritagya3"
+console.log(a)
+{
+    let b = "Hello"
+    console.log(b)  
+}
+console.log(b)  
