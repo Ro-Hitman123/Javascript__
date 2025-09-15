5c3846f350e0678a1918afc6a82bef98ea0af9d4
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 19:51:55 +0545

    dlf

--- t5 Operators and Expressions.js
+++ t5 Operators and Expressions.js
@@ -7,3 +7,28 @@ console.log("The value of a * b is", a * b)
 console.log("The value of a / b is", a / b)
 console.log("The value of a**b is", a ** b)
 console.log("The value of a % b is", a % b)
+console.log("The value of ++a is", ++a)
+console.log("The value of --a is", --a)
+console.log("The value of a++ is", a++)
+console.log("The value of a-- is", a--)
+console.log(a)
+
+// Assignment operators
+let c = b
+c += 2
+console.log(c)
+
+// Comparison operators
+console.log(c == b)
+console.log(c >= b)
+console.log(c <= b)
+console.log(c > b)
+console.log(c < b)
+console.log(c != b)
+console.log(c === b) // This checks value as well as datatype.
+console.log(c !== b) // This checks value as well as datatype.
+
+// Logical operators
+console.log(c < b && c > b)
+console.log(c < b || c > b)
+console.log(!(c < b))
