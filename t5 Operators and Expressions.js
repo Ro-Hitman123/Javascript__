9f94bcfd43b3159bd3c77c9fe83785c6e2368512
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 19:34:42 +0545

    df

---
+++ t5 Operators and Expressions.js
@@ -0,0 +1,9 @@
+console.log("Operators in JavaScript")
+let a = 34
+let b = 5
+console.log("The value of a + b is", a + b)
+console.log("The value of a - b is", a - b)
+console.log("The value of a * b is", a * b)
+console.log("The value of a / b is", a / b)
+console.log("The value of a**b is", a ** b)
+console.log("The value of a % b is", a % b)
