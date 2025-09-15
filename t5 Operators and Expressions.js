262a104e0231aeff36002a1f226384328f9c1599
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-15 19:53:31 +0545

    sfkdj

--- t5 Operators and Expressions.js
+++ t5 Operators and Expressions.js
@@ -32,3 +32,12 @@ console.log(c !== b) // This checks value as well as datatype.
 console.log(c < b && c > b)
 console.log(c < b || c > b)
 console.log(!(c < b))
+console.log(!(c > b))
+
+// Create a variable of type string and try to add a number to it. Observe the results.
+let str = "Hello"
+let num = 5
+console.log(str + num)
+
+// Create typeof operator to find the datatype of the string in last question.
+console.log(typeof (str + num))
