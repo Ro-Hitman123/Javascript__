24b0f1f7966a54e5703d3759327f3eccc30ed8e8
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-16 23:52:14 +0545

    kfd

---
+++ t10 String.js
@@ -0,0 +1,12 @@
+let name = "Awesome"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name[3])
+console.log(name.indexOf("some"))
+
+let boy1 = "Saroj"
+let boy2 = "Paras"
+
+let friendship = `${boy1} is a friend of ${boy2}`
+console.log(friendship)
