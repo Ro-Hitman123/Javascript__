137131041fdcee14088540542483fbf0d0691188
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-17 00:00:20 +0545

    fkd

--- t10 String.js
+++ t10 String.js
@@ -8,7 +8,7 @@ console.log(name.indexOf("some"))
 // Template Literals
 let boy1 = "Saroj"
 let boy2 = "Paras"
-let friendship = `${boy1} is a friend of ${boy2}`
+let friendship = `${boy1} is a friend of ${boy2}`// String Interpolation in which we can use variable inside string or inside backticks
 console.log(friendship)
 
 //Escape sequence characters
