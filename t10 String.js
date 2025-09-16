a5d5842b95e6d23c8c11647a93dc403286371b6d
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-16 23:57:52 +0545

    lkjs

--- t10 String.js
+++ t10 String.js
@@ -5,8 +5,12 @@ console.log(name.toLowerCase())
 console.log(name[3])
 console.log(name.indexOf("some"))
 
+// Template Literals
 let boy1 = "Saroj"
 let boy2 = "Paras"
-
 let friendship = `${boy1} is a friend of ${boy2}`
 console.log(friendship)
+
+//Escape sequence characters
+let fruit = 'Bana\'na'
+console.log(fruit)
