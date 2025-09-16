033851279fd5159247add4e309dd09176a11bbcd
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-16 23:38:31 +0545

    skdfj

--- t9 practice set.js
+++ t9 practice set.js
@@ -3,7 +3,7 @@ let marks = {
     shubham: 78,
     harry: 99.977,
     rohan: 67,
-    skillf: 100
+    Ram: 100
 }
 for (let i = 0; i<Object.keys(marks).length; i++){
     console.log("The marks of " + Object.keys(marks)[i] + " is " + marks[Object.keys(marks)[i]])
@@ -14,3 +14,16 @@ for (let i = 0; i<Object.keys(marks).length; i++){
 for(let keys in marks){
     console.log("The marks of " + keys + " is " + marks[keys])
 }
+
+// Write a function to find mean of the 5 numbers
+let mean ={
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5
+}
+const numbers = () =>{
+    return(mean.a + mean.b + mean.c + mean.d + mean.e)/5
+}
+console.log("The mean of the 5 numbers is " + numbers())
