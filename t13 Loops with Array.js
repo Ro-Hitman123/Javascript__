da5c146015fb4923a4bec1ab1b21219e59781024
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-17 23:38:12 +0545

    kfj

--- t13 Loops with Array.js
+++ t13 Loops with Array.js
@@ -1,6 +1,6 @@
-let num = [3, 54, 1, 2, 4]
+let num = [3, 5, 1, 2, 4]
 for (let i = 0; i<num.length; i++){
-    console.log(num[i])
+    console.log(num[i]*num[i])
 }
 
 // forEach Loop
@@ -24,3 +24,27 @@ for (let i of num){
 for (let index in num){
     console.log(num[index])
 }
+
+// map() method is used to create a new array by performing some operation on each element of the array
+
+let newNum = [45, 23, 21]
+let a =newNum.map((value, index, array) => {
+    console.log(value, index, array)
+    return value + index
+})
+console.log(a) // new array is created by adding index to each element of the array
+
+// filter() method is used to create a new array by filtering out some elements of the array based on some condition
+
+let newNum2 = [45, 1, 21, 67, 34, 89]
+let b = newNum2.filter((value) => {
+    return value < 10
+})
+console.log(b) // new array is created by filtering out elements less than 10
+
+// reduce() method is used to reduce the array to a single value by performing some operation on each element of the array
+let newNum3 = [5, 6, 2, 3]
+let c = newNum3.reduce((h1, h2) => {
+ return h1 + h2
+})
+console.log(c) // returns the sum of all elements in the array
