0162580a0c2ef6a8d55ef70760aad985d9d988c3
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-19 23:20:13 +0545

    dfjk

--- t16.js
+++ t16.js
@@ -4,3 +4,27 @@ console.log(document.body)
 document.body.style.background = "blue"
 document.body.style.fontSize = "50px"
 // location.href = "https://www.google.com"
+
+let runAgain = true
+
+const canDrive = (age) => {
+    return age>=18? true: false
+}
+    while(runAgain){
+        let age = prompt("Enter your age")
+        age = Number.parseInt(age)
+if (age<0) {
+    console.error("Please enter a valid age")
+    break
+}
+if(canDrive(age)){
+    alert("You are allowed to drive the car")
+}
+else{
+    alert("You are not allowed to drive the car")
+}
+runAgain = confirm("Do you want to run again?")
+}
+
+let color = prompt("Enter a color")
+document.body.style.background = color
