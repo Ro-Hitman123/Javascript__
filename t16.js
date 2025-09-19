82e1390b8571fb9d47fd9ac05b83a6b5f63c0d97
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-19 22:49:40 +0545

    dfkj

---
+++ t16.js
@@ -0,0 +1,6 @@
+console.log(window)
+console.log(document) // DOM is the document object model
+console.log(document.body)
+document.body.style.background = "blue"
+document.body.style.fontSize = "50px"
+// location.href = "https://www.google.com"
