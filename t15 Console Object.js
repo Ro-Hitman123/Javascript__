347454fb0daf1f36bb6c1351c29c8fcf9ade9bb5
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-19 21:53:08 +0545

    fdks

--- t15 Console Object.js
+++ t15 Console Object.js
@@ -6,3 +6,9 @@ let obj = {name: "John", age: 30, city: "New York"}
 console.table(obj)
 console.warn("hey please don't do that!")
 console.info("Hey this is an important information")
+console.time("Your code took")
+console.timeEnd("Your code took")
+console.time("for Loop")
+for(let i=0; i<1000000; i++){}
+console.timeEnd("for Loop")
+console.trace("Here is the trace")
