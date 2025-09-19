128b00c8b0aee4c0cade6c0db6999bc484df7bb1
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-19 21:46:06 +0545

    dfjh

--- t15 Console Object.js
+++ t15 Console Object.js
@@ -1,3 +1,8 @@
 console.log("Hey there!")
 console.error("This is an error message")
 console.assert(5>53)
+// console.clear()
+let obj = {name: "John", age: 30, city: "New York"}
+console.table(obj)
+console.warn("hey please don't do that!")
+console.info("Hey this is an important information")
