31ac009c829d8b537b4ba2da4001ffa05a9082dc
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-21 18:53:36 +0545

    dfjk

--- t18 Walking the DOM.js
+++ t18 Walking the DOM.js
@@ -1,10 +1,10 @@
-// Walking the DOM
-document.body.style.background = "lightblue"
-document.body.style.fontSize = "20px"           
-document.body.style.fontFamily = "Arial, sans-serif"
-document.body.style.color = "darkblue"
-document.body.style.padding = "20px"
-document.head
-document.body
-document.documentElement
-document.title // it returns string
+// // Walking the DOM
+// document.body.style.background = "lightblue"
+// document.body.style.fontSize = "20px"           
+// document.body.style.fontFamily = "Arial, sans-serif"
+// document.body.style.color = "darkblue"
+// document.body.style.padding = "20px"
+// document.head
+// document.body
+// document.documentElement
+// document.title // it returns string
---
+++ t19 Accessing Children of an Element.js
@@ -0,0 +1,10 @@
+// // Accessing Children of an Element
+// let element = document.body
+// console.log(element.firstChild)
+// console.log(element.lastChild)
+// console.log(element.childNodes)
+// console.log(element.children)
+// console.log(element.childElementCount)
+// console.log(element.children[0])
+// console.log(element.children[element.children.length - 1])
+// console.log(element.children[2])
