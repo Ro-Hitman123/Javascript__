06f82c7d9022f4d1c933e0dd3f4270a3b63d31d5
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-09-22 17:14:52 +0545

    gij

--- t20.js
+++ t20.js
@@ -1,3 +1,3 @@
-let b = document.body
-console.log("First child of b is: ", b.firstChild)
-console.log("First Element child of b is: ", b.firstElementChild)
+// let b = document.body
+// console.log("First child of b is: ", b.firstChild)
+// console.log("First Element child of b is: ", b.firstElementChild)
---
+++ t21 Table Navigation.js
@@ -0,0 +1,5 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.thead)
+console.log(t.row)
+console.log(t.col)
