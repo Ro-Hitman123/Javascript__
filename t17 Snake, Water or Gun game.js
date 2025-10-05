04605f7f906d1b782e84eedfbc0c1216f81a6309
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-10-05 16:03:16 +0545

    sgfdg

--- t17 Snake, Water or Gun game.js
+++ t17 Snake, Water or Gun game.js
@@ -1,32 +1,32 @@
-// document.write("Welcome to Snake, Water, Gun game! <br>")
-// document.write("S for Snake, W for Water and G for Gun <br>")
-// let user = prompt("Enter S, W or G")
-// let cpuI = Math.floor(Math.random()*3)
-// let cpu = ["S", "W", "G"][cpuI]
+document.write("Welcome to Snake, Water, Gun game! <br>")
+document.write("S for Snake, W for Water and G for Gun <br>")
+let user = prompt("Enter S, W or G")
+let cpuI = Math.floor(Math.random()*3)
+let cpu = ["S", "W", "G"][cpuI]
 
-// if (user !== "S" && user !== "W" && user !== "G") {
-//     document.write("Invalid input! Please enter S, W, or G.")
-// } else {
-//     const match = (cpu, user) => {
-//         if (user === cpu) {
-//             return("It's a tie!")
-//         } 
-//         else if (
-//             (user === "S" && cpu === "W") ||
-//             (user === "W" && cpu === "G") ||
-//             (user === "G" && cpu === "S")
-//         ) {
-//             return("user")
-//         } 
-//         else {
-//             return("cpu")
-//         }
-//     }
+if (user !== "S" && user !== "W" && user !== "G") {
+    document.write("Invalid input! Please enter S, W, or G.")
+} else {
+    const match = (cpu, user) => {
+        if (user === cpu) {
+            return("It's a tie!")
+        } 
+        else if (
+            (user === "S" && cpu === "W") ||
+            (user === "W" && cpu === "G") ||
+            (user === "G" && cpu === "S")
+        ) {
+            return("user")
+        } 
+        else {
+            return("cpu")
+        }
+    }
 
-//     let result = match(cpu, user)
-//     document.write(`You chose ${user}<br>CPU chose ${cpu}<br>The winner is : ${result}`)
-//     let color = prompt("Enter a bg color")
-//     document.body.style.background = color
-//     document.body.style.fontSize = "20px"
-// }
+    let result = match(cpu, user)
+    document.write(`You chose ${user}<br>CPU chose ${cpu}<br>The winner is : ${result}`)
+    let color = prompt("Enter a bg color")
+    document.body.style.background = color
+    document.body.style.fontSize = "20px"
+}
 
---
+++ t26.js
@@ -0,0 +1,2 @@
+// console.dir() shows element as an object with its propertiess
+
